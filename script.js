ccd50bb18ddecd83250d848a9dbb66160ce59553
Kush Surti <kushsurti05@gmail.com>
2022-12-19 11:36:54 +0530

    added counting functionality

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your code here
+const textArea = document.querySelector("#evaluatedText");
+const letterCount = document.querySelector("#letterCount");
+textArea.addEventListener("keyup", calculateLettersCount);
+
+function calculateLettersCount(event) {
+  letterCount.innerText = event.target.value.length;
+}
